ed9d77dd0128fd4abe28c95dd798216c5e27d578
PJEun <91645374+PJEun@users.noreply.github.com>
2022-02-10 20:50:22 +0900

    Add files via upload

---
+++ src/util/EnvBeige.java
@@ -0,0 +1,25 @@
+package util;
+
+public class EnvBeige {
+	/*
+	 * 결제 서비스(I'mport)
+	 * https://www.iamport.kr/
+	 */
+	
+	/* 가맹점 식별코드 */
+	public static String iamportKey = "";
+	
+	
+	
+	/*
+	 * 문자 전송 서비스 (CoolSMS)
+	 * https://coolsms.co.kr/
+	 */
+	
+	/* API Key */
+	public static String coolsmsApiKey = "";
+	/* API Secret */
+	public static String coolsmsApiSecret = "";
+	/* 발신 번호 */
+	public static String coolsmsNumber = "";
+}
